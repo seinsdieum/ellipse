0999b50fe53d7768247dec4ca19aa92ca75e7dfb
vapeEllipse <geometryfleezed@gmail.com>
2022-12-28 18:45:29 +0300

    js addition + screen adapt 0.2

--- page/base/index.js
+++ page/base/index.js
@@ -1,31 +1,74 @@
 const [red, green, blue] = [34, 34, 34]
 const headers = document.querySelectorAll('header')
 
-const text = 'kek lol karvalol'
-const textElement = document.createElement('h1')
-textElement.append(text)
-const menuInfo = document.createElement('div.row')
-menuInfo.appendChild(textElement)
+function rowPoint()
+{
+    let row = document.createElement('div')
+    row.className = 'row'
+    return row
+}
+
+function text(str) {
+    let textCase = document.createElement('p')
+    textCase.append(str)
+    return textCase
+}
+
+const product = rowPoint()
+product.style.backgroundColor = 'black'
+product.appendChild(text('lalala'));
+
+const newPoint = rowPoint()
+newPoint.style.backgroundColor = 'yellow'
+newPoint.append(text('krasava'))
+
+const company = rowPoint()
+company.style.backgroundColor = 'red'
+
 const element = document.querySelector('article')
 
 let changeableElement;
 
-console.log(headers)
-window.addEventListener('scroll', () => {
-    let y = 1 + (window.scrollY || window.pageYOffset) / 500
-    y = y < 1 ? 1 : y // ensure y is always >= 1 (due to Safari's elastic scroll)
-    const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
-    for(let header of headers) {
-        header.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
-    }
+let productClicked = false;
+let newClicked = false;
+let companyClicked = false;
+let supportClicked = false;
+let eventClicked = false;
 
-})
 
-function showTestMenu() {
+function showProductMenu() {
     changeableElement = element.firstChild
-    element.firstChild.replaceWith(menuInfo)
+    element.firstChild.replaceWith(product)
+    console.log(product)
 }
+
+function showNewMenu() {
+    changeableElement = element.firstChild
+    element.firstChild.replaceWith(newPoint)
+    console.log(newPoint)
+}
+
+function showCompanyMenu() {
+    changeableElement = element.firstChild
+    element.firstChild.replaceWith(company)
+    console.log(company)
+}
+
+
 
 function disposeTestMenu() {
     element.firstChild.replaceWith(changeableElement)
 }
+const warning = document.querySelector('header.warning')
+window.addEventListener('scroll', () => {
+    let y = 1 + (window.scrollY || window.pageYOffset) / window.innerHeight
+
+    y = y < 1 ? 1 : y // ensure y is always >= 1 (due to Safari's elastic scroll)
+    const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
+    for(let header of headers) {
+        header.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+
+    }
+    warning.style.opacity = `${0}`
+
+})
