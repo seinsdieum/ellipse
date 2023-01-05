a5585d98c7d2fe6bfc747ce3184a9b45d37b63b3
seinsdieum <116916029+seinsdieum@users.noreply.github.com>
2023-01-05 22:54:28 +0300

    product menu

--- page/base/productMenu.js
+++ page/base/productMenu.js
@@ -1,5 +1,4 @@
 
-
 function button(str, borders) {
     const butt = document.createElement('button')
     if(borders) {
@@ -7,32 +6,51 @@ function button(str, borders) {
     } else {
         butt.className = 'text-navigation'
     }
-    butt.append(text(str))
-    return button
+    /*if(window.innerWidth > 768) {
+        butt.style.maxHeight = '10vh'
+    } else {
+        butt.style.maxHeight = '5vh'
+        butt.style.maxWidth = '15vw'
+        butt.style.margin = '1vw'
+    }*/
+    butt.append(header(str))
+    return butt
+
 }
 
 function buttonList(horizontal, _buttonList) {
     const buttonLst = document.createElement('ul')
     buttonLst.className = 'text-button-list'
-    if (!horizontal) {
-        buttonLst.id = 'vertical'
-    }
-    for(let bu of _buttonList) {
-        const li = document.createElement('li')
-        li.append(bu)
-        buttonLst.append(li)
+    for(let bu of Object.values(_buttonList)) {
+        let li = document.createElement('li')
+        li.appendChild(bu)
+        buttonLst.appendChild(li)
     }
     return buttonLst
 }
 
 function addMenuItems() {
-    const item1Button = button('tmt-46', false)
-    const item2Button = button('tmt-46', false)
-    const item3Button = button('tmt-46', false)
-    const item4Button = button('tmt-46', false)
+    const item1Button = button('TMT-46', false)
+    const item2Button = button('Verigon I', false)
+    const item3Button = button('TMT-45', false)
+    const item4Button = button('Kali', false)
 
-    const menuList = buttonList(false, {item1Button, item2Button, item3Button, item4Button})
-    product.appendChild(menuList)
+    const bntLst = {item1Button, item2Button, item3Button, item4Button}
+    const menuList = buttonList(false, bntLst)
+
+    const testButt = button('lolkaa', false)
+
+    const row1 = row()
+    const row2 = row()
+
+    row1.append(menuList)
+    row2.append(testButt)
+    if(product.firstChild === null) {
+        product.appendChild(row1)
+        product.appendChild(row2)
+    }
 }
 
-product.addEventListener('load', addMenuItems)
+console.log(product)
+
+productButton[0].addEventListener('mouseover', addMenuItems)
